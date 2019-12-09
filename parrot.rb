a3927f2d0b1e3c6129bfe00b7c6e6042860d964b
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-09 18:51:15 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(sound = "Squawk")
+def parrot(phrase = "Squawk")
   
-  
+  puts "Parrot is a #{phrase}."
   
   
   
